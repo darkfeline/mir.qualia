42548cdeb9c97052f77acb0a3f1a0c1fb51bdd3e
Allen Li <darkfeline@felesatra.moe>
2016-09-08 22:26:21 -0700

    Fix stuff

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ from setuptools import find_packages, setup
 setup(
     name='qualia',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=['qualia_tests*']),
     entry_points={
-        'qualia': 'qualia.__main__:main',
+        'console_scripts': [
+            'qualia = qualia.__main__:main',
+        ]
     },
 
     author='Allen Li',
