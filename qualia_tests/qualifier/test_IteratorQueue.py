7017ec4363e14a30b4347eed8c5432d7dba0b5b8
Allen Li <darkfeline@felesatra.moe>
2016-09-09 00:51:04 -0700

    Add tests

---
+++ qualia_tests/qualifier/test_IteratorQueue.py
@@ -0,0 +1,21 @@
+from qualia import qualifier
+
+
+def test_iteration():
+    queue = qualifier.IteratorQueue([1, 2, 3])
+    assert list(queue) == [1, 2, 3]
+    assert list(queue) == []
+
+
+def test_reiteration():
+    queue = qualifier.IteratorQueue()
+    assert list(queue) == []
+    queue.extend([1, 2, 3])
+    assert list(queue) == [1, 2, 3]
+
+
+def test_multiple_extend():
+    queue = qualifier.IteratorQueue()
+    queue.extend([1])
+    queue.extend([2])
+    assert list(queue) == [1, 2]
