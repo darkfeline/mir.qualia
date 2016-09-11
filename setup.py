864abb30fae3260a988e7c5bf3b9251bfaabe77f
Allen Li <darkfeline@felesatra.moe>
2016-09-11 02:35:39 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from setuptools import setup
 
 setup(
     name='mir.qualia',
-    version='0.1.0',
-    description='Enable sections of config files for different machines.',
+    version='0.2.0',
+    description='Dynamically enable sections of config files.',
     long_description='',
     keywords='',
     url='https://github.com/darkfeline/mir.qualia',
     author='Allen Li',
     author_email='darkfeline@felesatra.moe',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: End Users/Desktop',
         'Programming Language :: Python :: 3.5',
