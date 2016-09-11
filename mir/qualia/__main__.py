2b2a0d6fec31598f48127508166ed4abfc47d154
Allen Li <darkfeline@felesatra.moe>
2016-09-11 02:28:34 -0700

    Implement main

--- mir/qualia/__main__.py
+++ mir/qualia/__main__.py
@@ -1,5 +1,16 @@
+import argparse
+import sys
+
+from mir.qualia import qualifier
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('qualities', nargs='*')
+    args = parser.parse_args()
+    qual = qualifier.Qualifier(args.qualities)
+    for line in qual(sys.stdin):
+        sys.stdout.write(line)
 
 if __name__ == '__main__':
     main()
